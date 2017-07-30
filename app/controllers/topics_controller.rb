804e129a7fee29326768b54267a5ab89d5681d33
fumidesu <fumilein23@yahoo.co.jp>
2017-07-30 08:48:50 +0200

    first commit

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,50 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only: [:edit, :update, :destroy]
+
+
+  def index
+    @topics = Topic.all
+  end
+
+  def create
+    @topic = Topic.new(topics_params)
+    if @topic.save
+    redirect_to topics_path, notice: "トピックを作成しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @topic.update(topics_params)
+      redirect_to topics_path, notice: "トピックを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to topics_path, notice: "トピックを削除しました！"
+  end
+
+  private
+      def topics_params
+        params.require(:topic).permit(:title, :content)
+      end
+
+      def set_topic
+           @topic = Topic.find(params[:id])
+         end
+
+end
