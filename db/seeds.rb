5f32d02b8b9a97d1b89a7b0c89e884e6d80e1912
fumidesu <fumilein23@yahoo.co.jp>
2017-07-30 17:47:15 +0200

    dive9

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  nutzer = User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               )
+  Topic.create!(title: "AAA",
+               content: "BBB",
+               user_id: nutzer.id
+               )
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
