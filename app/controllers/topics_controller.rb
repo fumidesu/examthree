d40cd73ad9ddf4136eefe678346cd52bd247ca51
fumidesu <fumilein23@yahoo.co.jp>
2017-08-05 16:48:38 +0200

    massage

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,16 @@
 class TopicsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_topic, only: [:edit, :update, :destroy]
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
 
   def index
     @topics = Topic.all
+
+  end
+
+  def show
+    @comment = @topic.comments.build
+    @comments = @topic.comments
   end
 
   def create
@@ -23,9 +29,6 @@ class TopicsController < ApplicationController
   end
 
   def edit
-  end
-
-  def show
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
 Rails.application.routes.draw do
+
+  #get 'relationships/create'
+
+  #get 'relationships/destroy'
+
   devise_for :users, controllers: {
     registrations: "users/registrations",
      omniauth_callbacks: "users/omniauth_callbacks"
    }
-   
-  resources :topics, only: [:index, :new, :create, :edit, :update ,:destroy]
+
 root 'topics#index'
+resources :topics do
+  resources :comments
+  post :confirm, on: :collection
+end
 
+resources :users, only: [:index, :show]
+resources :relationships, only: [:create, :destroy]
 
+resources :conversations do
+  resources :messages
+end
 
 if Rails.env.development?
 mount LetterOpenerWeb::Engine, at: "/letter_opener"
